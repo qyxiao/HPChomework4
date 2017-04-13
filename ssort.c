ff38ab8ed67da8f81b49813cfcfe8fc90970f4ba
qyxiao <qx344@nyu.edu>
2017-04-13 11:11:25 +0800

    add job, add time to ssort

--- ssort.c
+++ ssort.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <mpi.h>
 #include <stdlib.h>
-
+#include "util.h"
 
 static int compare(const void *a, const void *b)
 {
@@ -31,34 +31,44 @@ int main( int argc, char *argv[])
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   /* Number of random numbers per processor (this should be increased
    * for actual tests or could be passed in through the command line */
-  N = 100;
   
+  N = 100; 
+  if(argc>=2){
+    sscanf(argv[1], "%d", &N);
+  }
+  
+  
+  
+  timestamp_type time1, time2;
+  get_timestamp(&time1);
+
   vec = calloc(N, sizeof(int));
   /* seed random number generator differently on every core */
-  srand((unsigned int) (rank + 393919));
+  srand((unsigned int) (rank+ 393919));
 
   /* fill vector with random integers */
   for (i = 0; i < N; ++i) {
     vec[i] = rand();
   }
-  printf("rank: %d, first entry: %d\n", rank, vec[0]);
+
+  /* printf("rank: %d, first entry: %d\n", rank, vec[0]);  */
   qsort(vec, N, sizeof(int), compare);
     
   svec = calloc(gap, sizeof(int));
   for(i=1;i<=gap;i++){
-      svec[i-1]=vec[(N/gap)*i];
+      svec[i-1]=vec[(N/gap)*i-1];
   }
 
   if(rank==0){
     rvec = calloc(size*gap, sizeof(int));
   }
 
-  MPI_Gather(svec,gap,MPI_INT,rvec,size*gap,MPI_INT,0,MPI_COMM_WORLD);
+  MPI_Gather(svec,gap,MPI_INT,rvec,gap,MPI_INT,0,MPI_COMM_WORLD);
   spliter = calloc(size-1, sizeof(int));
 
   if(rank==0){
     qsort(rvec, gap*size, sizeof(int), compare);  
-    for(i=0,i<size-1,i++){
+    for(i=0;i<size-1;i++){
       spliter[i]=rvec[(i+1)*gap];
     }
   }
@@ -66,20 +76,28 @@ int main( int argc, char *argv[])
   MPI_Bcast(spliter,size-1,MPI_INT,0,MPI_COMM_WORLD);
   counter = calloc(size, sizeof(int));
   Rcounter = calloc(size, sizeof(int));
+  
   subsum = 0;
   index = 0;
-  for(i=0;i<N;i++){
-    if(vec[i]>spliter[index]){
-      counter[index]=subsum;
-      subsum=0;
-      index++;
-    }else{
-      subsum++;
-    }
+  i=0;
+  while(i<N && index<size-1){
+     if(vec[i] <= spliter[index]){
+        i++;
+        subsum++;
+     }else{
+        counter[index]=subsum;
+        
+        subsum=0;
+        index++;
+     } 
   }
-  if(index<N){
+
+  if(index==size-1){
+    counter[index] = N-i;
+  }else{
     counter[index]=subsum;
   }
+
 
 
   MPI_Alltoall(counter,1,MPI_INT,Rcounter,1,MPI_INT,MPI_COMM_WORLD);
@@ -114,6 +132,7 @@ int main( int argc, char *argv[])
   
   
   MPI_Waitall(subsum, request_in, status_in);
+  MPI_Waitall(N, request_out, status_out);
   qsort(receiver, subsum, sizeof(int), compare);
   {
     FILE* fd = NULL;
@@ -124,17 +143,21 @@ int main( int argc, char *argv[])
       fprintf(fd, "  %d\n", receiver[i]);
 
     fclose(fd);
-    printf("Array received at the end is stored in output%d.txt\n", rank);
+
+    get_timestamp(&time2);
+    double elapsed = timestamp_diff_in_seconds(time1,time2);
+    printf("Array received at the end is stored in output%d.txt, time elapsed is %f seconds.\n", rank, elapsed);
   }
-  MPI_Waitall(N, request_out, status_out);
+
+  
   /* do a local sort */
 
   /* every processor writes its result to a file */
 
-  free(vec);free(svec);free(rvec);free(spliter);free(counter);
-  free(Rcounter);free(receiver);free(request_out);free(request_in);
-  free(status_out);free(status_in);
-
+  free(vec);free(svec);free(spliter);
+  free(counter);free(Rcounter);free(receiver);
+  if(rank==0)
+    free(rvec);
   MPI_Finalize();
   return 0;
 }
